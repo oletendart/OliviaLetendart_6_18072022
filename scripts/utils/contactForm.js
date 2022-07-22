cee55b5c01d55d282a654c35310de1ac2f28914f
Letendart Olivia <olivia.letendart@socrate-edu.com>
2022-07-22 15:46:01 +0200

    Create html and css for the page photographer and index + create and modificate modal form and lightbox

--- scripts/utils/contactForm.js
+++ scripts/utils/contactForm.js
@@ -1,9 +1,23 @@
 function displayModal() {
     const modal = document.getElementById("contact_modal");
+    const body = document.body;
 	modal.style.display = "block";
+    console.log(body);
+    body.style.backgroundColor = "#C4C4C466";
 }
 
 function closeModal() {
     const modal = document.getElementById("contact_modal");
+    const body = document.body;
+    modal.style.display = "none";
+    body.style.backgroundColor = "transparent";
+}
+
+function displayLightbox() {
+    const modal = document.getElementById("lightbox_modal");
+    modal.style.display = "block";
+}
+function closeLightbox() {
+    const modal = document.getElementById("lightbox_modal");
     modal.style.display = "none";
 }
