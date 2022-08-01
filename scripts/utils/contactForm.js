dd14fcab3f0ac8ab064bb161dff985f142ea8507
Letendart Olivia <olivia.letendart@socrate-edu.com>
2022-08-01 17:05:42 +0200

    Modificate lightbox and crate button contains three span

--- scripts/utils/contactForm.js
+++ scripts/utils/contactForm.js
@@ -1,16 +1,11 @@
 function displayModal() {
     const modal = document.getElementById("contact_modal");
-    const body = document.body;
 	modal.style.display = "block";
-    console.log(body);
-    body.style.backgroundColor = "#C4C4C466";
 }
 
 function closeModal() {
     const modal = document.getElementById("contact_modal");
-    const body = document.body;
     modal.style.display = "none";
-    body.style.backgroundColor = "transparent";
 }
 
 function displayLightbox() {
